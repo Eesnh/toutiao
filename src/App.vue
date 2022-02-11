e14ab595761ee449e191b722b65dde4787d293c8
H <huachunfan@yeah.net>
2022-02-11 16:43:15 +0800

    引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div id="app">
     <h1>头条</h1>
+    <div>
+      <i class="iconfont icon-pinglun"></i>
+    </div>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
     <router-view/>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+  }
 }
 </script>
 
