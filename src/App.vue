eebd9333b93c972256cd8503c287166803a881a4
H <huachunfan@yeah.net>
2022-02-12 14:49:19 +0800

    创建登录组件并设置路由

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
   <div id="app">
-    <h1>头条</h1>
-    <div>
-      <i class="iconfont icon-pinglun"></i>
-    </div>
-
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="success">成功按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box"></div>
     <router-view/>
   </div>
 </template>
@@ -25,9 +14,5 @@ export default {
 </script>
 
 <style lang="less">
-.box{
-  width: 750px;
-  height: 100px;
-  background-color: aqua;
-}
+
 </style>
