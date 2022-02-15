42ca20044feb8f50cd8f395d8c3916b7bcdc9875
H <huachunfan@yeah.net>
2022-02-15 11:12:55 +0800

    创建tabbar结构

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,30 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('@/views/login')
+  },
+  {
+    path: '/',
+    name: '', // 设置了子路由，需要设置为空，再设置name没有意义
+    component: () => import('@/views/layout'),
+    children: [
+      {
+        path: '', // 设置为空，默认是子路由，只能有一个
+        name: 'home',
+        component: () => import('@/views/home')
+      }, {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa')
+      }, {
+        path: '/videos',
+        name: 'videos',
+        component: () => import('@/views/videos')
+      }, {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my')
+      }
+    ]
   }
 ]
 
